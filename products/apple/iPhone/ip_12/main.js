7365ad101166113dda140234e64c8e66b00e1240
Saheer Babu <sheerbabu549@gmail.com>
2024-04-18 12:43:03 +0530

    db aded

--- products/apple/iPhone/ip_12/main.js
+++ products/apple/iPhone/ip_12/main.js
@@ -1,6 +1,48 @@
 
 
 
+
+const firebaseConfig = {
+  apiKey: "AIzaSyCsTD5XSRNl7VG-i6Ir0F3D1X1PxWk2Rfs",
+  authDomain: "shopify-30670.firebaseapp.com",
+  databaseURL: "https://shopify-30670-default-rtdb.firebaseio.com",
+  projectId: "shopify-30670",
+  storageBucket: "shopify-30670.appspot.com",
+  messagingSenderId: "792157900529",
+  appId: "1:792157900529:web:32d02d2d8b3fe05d94e350",
+  measurementId: "G-MZC38NN5BZ"
+};
+
+
+firebase.initializeApp(firebaseConfig);
+var database = firebase.database();
+var caseRef = firebase.database().ref('shopless/products/apple/ip_12/');
+
+
+
+
+caseRef.on('child_added', function(snapshot) {
+  // Get the data for the newly added child
+  var data = snapshot.val();
+  
+  //console.log(data.product_image)
+let products = document.querySelector(".products1")
+
+products.innerHTML+=`
+<a href="ip_8/" class="product">
+                            <div class="product__image-wrapper">
+                                <img src="${data.product_image}" alt="Mac">
+                            </div>
+                            ${data.product_name}
+                        </a>`
+
+
+  // Do something with the data
+  console.log('New child added:', data);
+});
+
+
+
 $(document).ready(function () {
     console.log('ready')
     document.title="Shopify";
@@ -12,7 +54,7 @@ $(document).ready(function () {
   
           document.querySelector(".dp1").src=dp
           
-      }
+      } 
       
       let userName = localStorage.getItem("shopify-name")
       if (userName!=null) {
