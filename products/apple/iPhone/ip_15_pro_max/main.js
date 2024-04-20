f4f164be0d5b6d8aff5242d63477efaadb6e23b3
Saheer Babu <sheerbabu549@gmail.com>
2024-04-20 14:54:50 +0530

    case thumb

--- products/apple/iPhone/ip_15_pro_max/main.js
+++ products/apple/iPhone/ip_15_pro_max/main.js
@@ -28,7 +28,7 @@ ref.on('child_added', function (snapshot) {
   let cableSec = document.querySelector(".productSec")
 
   cableSec.innerHTML += `
-  <a class="product_card" href="./cases/">
+  <a class="product_card" href="${data.href}">
   <img class="caseImage" src="${data.product_image}" alt="">
   <h2>${data.product_name}</h2>
 </a>`
