21575509b984e1f1cc0f8ef5ed685f12b6b5227d
Saheer Babu <sheerbabu549@gmail.com>
2024-04-23 22:16:25 +0530

    searchbar update

--- main.js
+++ main.js
@@ -461,6 +461,33 @@ var iphoneModels = [
   "iPhone 14", "iPhone 14 Plus", "iPhone 14 Pro", "iPhone 14 Pro Max",
   "iPhone 15", "iPhone 15 Plus", "iPhone 15 Pro", "iPhone 15 Pro Max"
 ];
+const androidModels = [
+  "Samsung Galaxy S21", "Google Pixel 5", "OnePlus 9",
+  "Xiaomi Mi 11", "Sony Xperia 1 II", "Oppo Find X3 Pro",
+  "Huawei P40 Pro", "Motorola Edge Plus", "Asus ROG Phone 5",
+  "Realme GT", "Vivo X60 Pro", "Nokia 8.3 5G",
+  "LG Wing", "HTC U12+", "Lenovo Legion Phone Duel"
+];
+
+var allMobiles = [
+  "iPhone", "iPhone 3G", "iPhone 3GS", "iPhone 4", "iPhone 4s",
+  "iPhone 5", "iPhone 5c", "iPhone 5s", "iPhone 6", "iPhone 6 Plus",
+  "iPhone 6s", "iPhone 6s Plus", "iPhone SE (1st generation)", "iPhone 7",
+  "iPhone 7 Plus", "iPhone 8", "iPhone 8 Plus", "iPhone X", "iPhone XR",
+  "iPhone XS", "iPhone 11", "iPhone 11 Pro", "iPhone 11 Pro Max",
+  "iPhone SE (2nd generation)", "iPhone 12", "iPhone 12 mini",
+  "iPhone 12 Pro", "iPhone 12 Pro Max", "iPhone 13", "iPhone 13 mini",
+  "iPhone 13 Pro", "iPhone 13 Pro Max", "iPhone SE (3rd generation)",
+  "iPhone 14", "iPhone 14 Plus", "iPhone 14 Pro", "iPhone 14 Pro Max",
+  "iPhone 15", "iPhone 15 Plus", "iPhone 15 Pro", "iPhone 15 Pro Max", 
+
+  "Samsung Galaxy S21", "Google Pixel 5", "OnePlus 9",
+  "Xiaomi Mi 11", "Sony Xperia 1 II", "Oppo Find X3 Pro",
+  "Huawei P40 Pro", "Motorola Edge Plus", "Asus ROG Phone 5",
+  "Realme GT", "Vivo X60 Pro", "Nokia 8.3 5G",
+  "LG Wing", "HTC U12+", "Lenovo Legion Phone Duel"
+];
+
 
 /*initiate the autocomplete function on the "myInput" element, and pass along the countries array as possible autocomplete values:*/
-autocomplete(document.getElementById("search"), iphoneModels);
+autocomplete(document.getElementById("search"), allMobiles);
